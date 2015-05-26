2a5335cf1b344df52ce9720a47132eff660c77ee
uglycustard <uglycustard@buildergenerator.co.uk>
2015-05-26 22:11:30 +0100

    Quick fix for including property and class name when generation exception occurs.  Issue #16
    https://github.com/uglycustard/buildergenerator/issues/16

--- src/main/java/uk/co/buildergenerator/WithMethodFactory.java
+++ src/main/java/uk/co/buildergenerator/WithMethodFactory.java
@@ -15,19 +15,25 @@ class WithMethodFactory {
     
     WithMethod createWithMethod(PropertyDescriptor propertyDescriptor, Class<?> targetClass, String builderPackage, ClassesToIgnore classesToIgnore) {
         
-        String propertyName = bgu.getPropertyName(propertyDescriptor);
-        String parameterType = bgu.getParameterType(propertyDescriptor, builderPackage, classesToIgnore);
-        String parameterName = bgu.getParameterName(propertyDescriptor);
-        boolean collection = bgu.isCollection(propertyDescriptor);
-        boolean collectionNeedsInitialising = bgu.isCollectionNeedsInitialising(targetClass, propertyDescriptor);
-        String collectionGetterMethodName = bgu.getCollectionGetterMethodName(propertyDescriptor);
-        String collectionSetterMethodName = bgu.getCollectionSetterMethodName(propertyDescriptor);
-        String collectionTypeWhenCollectionNeedsInitialising = bgu.getCollectionTypeWhenCollectionNeedsInitialising(propertyDescriptor);
-        String collectionMethodSettingInvocation = bgu.getCollectionMethodSettingInvocation(propertyDescriptor, targetClass);
-        boolean builder = bgu.isBuilder(propertyDescriptor, classesToIgnore);
-        String builderTargetType = bgu.getBuilderTargetType(propertyDescriptor, classesToIgnore);
         
-        return new WithMethod(propertyName, parameterType, parameterName, collection, collectionNeedsInitialising, collectionGetterMethodName, collectionSetterMethodName, collectionTypeWhenCollectionNeedsInitialising, collectionMethodSettingInvocation, builder, builderTargetType);
+        try {
+            
+            String propertyName = bgu.getPropertyName(propertyDescriptor);
+            String parameterType = bgu.getParameterType(propertyDescriptor, builderPackage, classesToIgnore);
+            String parameterName = bgu.getParameterName(propertyDescriptor);
+            boolean collection = bgu.isCollection(propertyDescriptor);
+            boolean collectionNeedsInitialising = bgu.isCollectionNeedsInitialising(targetClass, propertyDescriptor);
+            String collectionGetterMethodName = bgu.getCollectionGetterMethodName(propertyDescriptor);
+            String collectionSetterMethodName = bgu.getCollectionSetterMethodName(propertyDescriptor);
+            String collectionTypeWhenCollectionNeedsInitialising = bgu.getCollectionTypeWhenCollectionNeedsInitialising(propertyDescriptor);
+            String collectionMethodSettingInvocation = bgu.getCollectionMethodSettingInvocation(propertyDescriptor, targetClass);
+            boolean builder = bgu.isBuilder(propertyDescriptor, classesToIgnore);
+            String builderTargetType = bgu.getBuilderTargetType(propertyDescriptor, classesToIgnore);
+            return new WithMethod(propertyName, parameterType, parameterName, collection, collectionNeedsInitialising, collectionGetterMethodName, collectionSetterMethodName, collectionTypeWhenCollectionNeedsInitialising, collectionMethodSettingInvocation, builder, builderTargetType);
+        } catch (RuntimeException e) {
+            
+            throw new RuntimeException(String.format("error generating builder method for property [%s] in class [%s], please log an issue at www.buildergenerator.co.uk", propertyDescriptor.getDisplayName(), targetClass.getName()), e);
+        }
     }
     
 }
